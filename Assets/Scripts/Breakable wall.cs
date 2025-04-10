728eac14e51ef781ca456a0a284654be2008f8d5
chlovers <chloie_day@hotmail.com>
2025-04-10 01:15:26 -0300

    adding second level

---
+++ Assets/Scripts/Breakable wall.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakablewall : MonoBehaviour
+{
+
+    bool hammerpickedup;
+
+    public GameObject hammer;
+    public GameObject Wall;
+
+    private void Start()
+    {
+        hammerpickedup = false;
+    }
+
+    public float breakDistance = 3f;
+    public Transform player;
+
+    private void Update()
+    {
+        if (!hammer.activeSelf)
+        {
+            hammerpickedup = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && Wall != null && hammerpickedup)
+        {
+            float distance = Vector3.Distance(player.position, Wall.transform.position);
+            if (distance <= breakDistance)
+            {
+                Wall.SetActive(false);
+            }
+        }
+    }
+
+}
